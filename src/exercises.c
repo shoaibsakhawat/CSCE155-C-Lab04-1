6d6d87de0701236b93fa43065b02a91eaf7a8a68
shoaib sakhawat <shoaibkarlal23@gmail.com>
2023-10-23 01:06:09 +0500

    used for loop

--- src/exercises.c
+++ src/exercises.c
@@ -49,6 +49,14 @@ int main(int argc, char **argv) {
 
   //4. Print squares of numbers 1 thru n separated by a comma
   //   except for the last one; example: 1, 4, 9, 16, 25
+    for (int i = 1; i<= n; i++) {
+    int squre = i * i;
+    if (i == n) { 
+      printf ("%d\n", squre);
+    } else { 
+      printf ("%d", squre);
+    }
+  }
 
   //5. Compute the summation of numbers 1 thru n and print
   //   the result; example: 1 + 2 + 3 + 4 + 5 = 15
